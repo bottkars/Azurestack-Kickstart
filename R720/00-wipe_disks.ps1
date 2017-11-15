49c09e28501f64a47caf587dce54bbe7c892d7ea
Administrator <Administrator@dscfra.dell.local>
2017-11-15 13:15:54 +0100

    added disk wipe

---
+++ R720/00-wipe_disks.ps1
@@ -0,0 +1,7 @@
+﻿foreach ($number in 1..10)
+    {
+    $disk = get-disk -Number $number
+    $disk | Set-Disk -IsOffline:$false
+    $disk | set-disk -IsReadOnly:$false
+    $disk | Clear-Disk -RemoveData -RemoveOEM -Confirm:$false
+    }
