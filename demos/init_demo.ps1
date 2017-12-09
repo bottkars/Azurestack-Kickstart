8d60b84d3733d273ee38e243a7446a79a17b4be2
Bott <Karsten.Bott@emc.com>
2017-12-09 08:18:11 +0100

    added demo

---
+++ demos/init_demo.ps1
@@ -0,0 +1,26 @@
+### set you git username !!!!
+$global:gitusername = "bottkars"
+## create below repo in you git !!!!
+$repo = "my_appservice_html5_demo" 
+
+##do ot change the upstreams, we´re mirroring them"
+$upstream = "appservice_html5_demo"
+$upstream_repo = "https://github.com/bottkars/$upstream.git"
+Set-Location $Home
+
+git clone $upstream_repo
+New-Item -ItemType Directory "$Home/$repo"
+set-location "$HOME/$repo"
+git init
+set-content -path $HOME/$repo/README.md -Value "README for AppDemo"
+git add README.md
+git commit -m 'first commit'
+git remote add origin "https://github.com/$($global:gitusername)/$repo"
+git push origin master
+
+Set-Location "$HOME/$Upstream"
+git remote set-url --push origin "https://github.com/$($global:gitusername)/$repo.git"
+git push --mirror "https://github.com/$($global:gitusername)/$repo"
+
+Set-Location $HOME
+Remove-Item $upstream -Recurse -Force -Confirm:$false
