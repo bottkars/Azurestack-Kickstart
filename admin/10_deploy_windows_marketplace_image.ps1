e76a8d3836a8d151d8bc4d4a8cca57f0411ea7f2
Karsten Bott <Karsten.Bott@emc.com>
2017-12-13 21:05:09 +0100

    checked update

--- admin/10_deploy_windows_marketplace_image.ps1
+++ admin/10_deploy_windows_marketplace_image.ps1
@@ -1,7 +1,9 @@
 ﻿[CmdletBinding(HelpUri = "https://github.com/bottkars/azurestack-dsc")]
 param (
-[Parameter(ParameterSetName = "1", Mandatory = $false,Position = 1)][ValidateScript({ Test-Path -Path $_ })]$Download_Path="$HOME/Downloads",
+[Parameter(ParameterSetName = "1", Mandatory = $false,Position = 1)][ValidateScript({ Test-Path -Path $_ })]$ISOPath="$HOME/Downloads",
+[Parameter(ParameterSetName = "1", Mandatory = $false,Position = 2)][ValidateScript({ Test-Path -Path $_ })]$UpdatePath="$HOME/Downloads",
 [version]$sku_version = (date -Format yyyy.MM.dd).ToString()
+
 )
 #REQUIRES -Module AzureStack.Connect
 #REQUIRES -Module AzureStack.ComputeAdmin
@@ -9,24 +11,26 @@ $Latest_ISO = "http://care.dlservice.microsoft.com/dl/download/1/4/9/149D5452-9B
 # latest http://download.windowsupdate.com/d/msdownload/update/software/updt/2017/11/windows10.0-kb4051033-x64_6e6a9d355d051a231e289a6d7931dd8f979f8d0c.msu
 $Latest_CU = "http://download.windowsupdate.com/d/msdownload/update/software/updt/2017/11/windows10.0-kb4051033-x64_6e6a9d355d051a231e289a6d7931dd8f979f8d0c.msu"
 $update_file = split-path -leaf $Latest_CU
+$updateFilePath = Join-Path $UpdatePath $update_file
 $ISO_FILE = Split-path -Leaf $Latest_ISO
-$ISOPath = Join-Path $Download_Path $ISO_FILE
+$ISOFilePath = Join-Path $ISOPath $ISO_FILE
 Write-Host -ForegroundColor White "[==>]Checking for $update_file" -NoNewline
-if (!(test-path ( Join-Path $Download_Path $update_file)))
+if (!(test-path ( Join-Path $UpdatePath $update_file)))
     {
-    Start-BitsTransfer -Description "Getting latest 2016CU" -Destination $Download_Path-Source $Latest_CU
+    Start-BitsTransfer -Description "Getting latest 2016CU" -Destination $UpdatePath -Source $Latest_CU
     }
 Write-Host -ForegroundColor Green [Done]
 Write-Host -ForegroundColor White "[==>]Checking for $ISO_FILE" -NoNewline
-If (!(test-path ( Join-Path $Download_Path $ISO_FILE)))
+If (!(test-path ( Join-Path $ISOFilePath)))
     {
-    Start-BitsTransfer -Description "Getting latest 2016CU" -Destination $Download_Path -Source $Latest_ISO
+    Start-BitsTransfer -Description "Getting latest 2016CU" -Destination $ISOPath -Source $Latest_ISO
     }
 Write-Host -ForegroundColor Green [Done]
+<#
 $GraphAudience = "https://graph.windows.net/"
 $TenantName = $Global:TenantName
 $ArmEndpoint = "https://adminmanagement.local.azurestack.external"
-<#
+
 # Create the Azure Stack operator's Azure Resource Manager environment by using the following cmdlet:
 Add-AzureRMEnvironment `
  -Name "AzureStackAdmin" `
@@ -41,4 +45,4 @@ $TenantID = Get-AzsDirectoryTenantId `
  -EnvironmentName AzureStackAdmin
 #>
 # Add a Windows Server 2016 Evaluation VM image.
-New-AzsServer2016VMImage -ISOPath $ISOPath -Version Both -CUPath $Updates_path/$update_file -CreateGalleryItem:$true -Location local -sku_version $SkuVersion
+New-AzsServer2016VMImage -ISOPath $ISOFilePath -Version Both -CUPath $updateFilePath -CreateGalleryItem:$true -Location local -sku_version $SkuVersion
