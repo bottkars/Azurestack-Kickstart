92996ae5b908a79c7777791987edddda34aab468
Karsten Bott <Karsten.Bott@emc.com>
2017-12-13 17:42:08 +0100

    admin for step 1

--- admin/01_post_deploy.ps1
+++ admin/01_post_deploy.ps1
@@ -1,7 +1,31 @@
+
 [CmdletBinding(HelpUri = "https://github.com/bottkars/azurestack-dsc")]
 param (
 [switch]$noutils
 )
+
+$myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+$myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+
+$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+ 
+# Check to see if we are currently running "as Administrator"
+if (!$myWindowsPrincipal.IsInRole($adminRole))
+  {
+  if ($noutils.IsPresent)
+    {
+        $arguments = "-noutils"
+    }
+  $arguments = "-Defaultsfile $Defaultsfile"
+  $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+  $newProcess.Arguments = "-noexit $PSScriptRoot/$($myinvocation.MyCommand) $arguments" 
+  Write-Host $newProcess.Arguments
+  $newProcess.Verb = "runas"
+  [System.Diagnostics.Process]::Start($newProcess)
+  exit
+  }
+
+
 Write-Host -ForegroundColor White -NoNewline  "[==>]Disabling WIndows Update"    
 Start-Process "sc" -ArgumentList "config wuauserv start=disabled" -Wait -NoNewWindow
 Write-Host -ForegroundColor Green "[Done]"
