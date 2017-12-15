629c2fedfb08c47c1dbc87ad598c470508431a87
Karsten Bott <Karsten.Bott@emc.com>
2017-12-15 12:13:57 +0100

    export root cert

---
+++ admin/tools/export_root_cert.ps1
@@ -0,0 +1,14 @@
+$label = "AzureStackSelfSignedRootCert"
+Write-Host "Getting certificate from the current user trusted store with subject CN=$label"
+$root = Get-ChildItem Cert:\CurrentUser\Root | Where-Object Subject -eq "CN=$label" | select -First 1
+if (-not $root)
+{
+    Log-Error "Certificate with subject CN=$label not found"
+    return
+}
+
+Write-Host "Exporting certificate"
+Export-Certificate -Type CERT -FilePath root.cer -Cert $root
+
+Write-Host "Converting certificate to PEM format"
+certutil -encode root.cer root.pem
