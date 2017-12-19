300c35074dd6ab7c3e58296d114106863d64296d
Karsten Bott <Karsten.Bott@emc.com>
2017-12-19 16:58:33 +0100

    first draft appservice workertier scaler

---
+++ admin/tools/scale-appservice-tier.ps1
@@ -0,0 +1,10 @@
+[CmdletBinding(HelpUri = "https://github.com/bottkars/azurestack-dsc")]
+param (
+[Parameter(ParameterSetName = "1", Mandatory = $false, Position = 1,ValueFromPipelineByPropertyName = $true)][ValidateSet(  
+'MediumWorkerTierScaleSet','MediumWorkerTierScale','MediumWorkerTierScale')]$WorkerTier,
+[int][ValidateRange(1.10)]$scale,
+[string]$ResourceGroup = "Appservice.local"
+)
+$rmvms = Get-AzureRmVmss -ResourceGroupName $ResourceGroup -VMScaleSetName $WorkerTier
+$rmvms.Sku.Capacity = $scale
+$rmvms | Update-AzureRmVmss
