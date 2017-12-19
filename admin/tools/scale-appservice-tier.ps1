f7fdf9260bd03880e54b4b61da56209a7debf176
Karsten Bott <Karsten.Bott@emc.com>
2017-12-19 17:00:14 +0100

    scaleset

--- admin/tools/scale-appservice-tier.ps1
+++ admin/tools/scale-appservice-tier.ps1
@@ -2,7 +2,7 @@
 param (
 [Parameter(ParameterSetName = "1", Mandatory = $false, Position = 1,ValueFromPipelineByPropertyName = $true)][ValidateSet(  
 'MediumWorkerTierScaleSet','MediumWorkerTierScale','MediumWorkerTierScale')]$WorkerTier,
-[int][ValidateRange(1.10)]$scale,
+[int][ValidateRange(1,10)]$scale,
 [string]$ResourceGroup = "Appservice.local"
 )
 $rmvms = Get-AzureRmVmss -ResourceGroupName $ResourceGroup -VMScaleSetName $WorkerTier
