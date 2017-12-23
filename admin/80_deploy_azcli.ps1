f6d44168b78d2c2a4fd95c9dddf2a2162b99918a
Karsten Bott <Karsten.Bott@emc.com>
2017-12-23 20:16:21 +0100

    azcli

---
+++ admin/80_deploy_azcli.ps1
@@ -0,0 +1,19 @@
+﻿param (
+$TMP_DIR = $HOME
+Remove-Item $SQL_DIR -Force -Recurse -Confirm:$false -ErrorAction SilentlyContinue | Out-Null
+$Uri = "https://aka.ms/InstallAzureCliWindows"
+$MSI = (Invoke-WebRequest -UseBasicParsing -MaximumRedirection 0 $Uri -ErrorAction SilentlyContinue).links.href
+Start-BitsTransfer $MSI
+<#
+$SQL_RP_FILE = Split-Path -Leaf $SQL_RP_URI
+Start-Process "./$SQL_RP_FILE" -ArgumentList "-s" -Wait
+$vmLocalAdminCreds = New-Object System.Management.Automation.PSCredential ($SQLRPadmin,$SQLRPPassword )
+$PfxPass = ConvertTo-SecureString $Global:VMPassword -AsPlainText -Force
+.\DeploySQLProvider.ps1 -AzCredential $Global:ServiceAdminCreds `
+  -VMLocalCredential $vmLocalAdminCreds `
+  -CloudAdminCredential $Global:cloudAdminCreds `
+  -PrivilegedEndpoint $global:privilegedEndpoint `
+  -DefaultSSLCertificatePassword $PfxPass `
+  -DependencyFilesLocalPath .\cert
+Pop-Location
+#>
