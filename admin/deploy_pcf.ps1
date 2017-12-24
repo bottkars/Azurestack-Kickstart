a28551db5a8cceb018ed5fe00afcb2f11f051a74
Karsten Bott <Karsten.Bott@emc.com>
2017-12-24 14:42:12 +0100

    latest chnages

--- admin/deploy_pcf.ps1
+++ admin/deploy_pcf.ps1
@@ -13,13 +13,18 @@ New-AzsTenantSubscription -DisplayName "Azure PCF Subscription" -Owner "Karsten 
 
 #$opsmanager_uri  = "https://opsmanagerwesteurope.blob.core.windows.net/images/ops-manager-$($ops_mgr_version).vhd"
 #Start-BitsTransfer -Source $opsmanager_uri -Destination C:\Temp #>
+
+
+<# register provider network storage fevault, compute "!!!!!! #>
+
 $RESOURCE_GROUP = "RG_PCF_MINIMUM"
 $LOCATION = $GLOBAL:AZS_Location
-New-AzureRmResourceGroup -ResourceGroupName $RESOURCE_GROUP -Location $LOCATION 
+
+$RSG = New-AzureRmResourceGroup -Name $RESOURCE_GROUP -Location $LOCATION
 $PCF_NSG = New-AzureRmNetworkSecurityGroup -Name "pfc-nsg" -ResourceGroupName $RESOURCE_GROUP -Location $LOCATION
 Add-AzureRmNetworkSecurityRuleConfig -Name ssh -NetworkSecurityGroup $PCF_NSG -Protocol Tcp -Priority 100 -DestinationPortRange 22
 Add-AzureRmNetworkSecurityRuleConfig -Name http -NetworkSecurityGroup $PCF_NSG -Protocol Tcp -Priority 200 -DestinationPortRange 80
-Add-AzureRmNetworkSecurityRuleConfig -Name https -NetworkSecurityGroup $PCF_NSG -Protocol Tcp -Priority 300 -DestinationPortRange 443
+
 
 ## OpsMGR NSG opsmgr-nsg
 $OPSMGR_NSG = New-AzureRmNetworkSecurityGroup -Name "opsmgr-nsg" -ResourceGroupName $RESOURCE_GROUP -Location $LOCATION
@@ -32,16 +37,17 @@ $vnet = New-AzureRmVirtualNetwork -Name pcf-net -ResourceGroupName $RESOURCE_GRO
 Add-AzureRmVirtualNetworkSubnetConfig -Name pcf -VirtualNetwork $vnet -AddressPrefix 10.0.0.0/20
 $Storage_Account = New-AzureRmStorageAccount -ResourceGroupName $RESOURCE_GROUP -Name "boshstorageaccount" -Type Standard_LRS -Location $LOCATION
 $OpsManage_Container = $Storage_Account | New-AzureStorageContainer -Name opsmanager -Permission Off
-$Storage_Account | New-AzureStorageContainer -Name bosh -Permission Off
-$Storage_Account | New-AzureStorageContainer -Name stemcell -Permission Blob
-$Storage_Account | New-AzureStorageTable -Name stemcells
+$Bosh_Container = $Storage_Account | New-AzureStorageContainer -Name bosh -Permission Off
+$Stemcell_Container =$Storage_Account | New-AzureStorageContainer -Name stemcell -Permission Blob
+$Stemcell_table = $Storage_Account | New-AzureStorageTable -Name stemcells
 
 
-Set-AzureStorageBlobContent -Container $OpsManage_Container -File 
-
-$Deployment_Storage_Account = New-AzureRmStorageAccount -ResourceGroupName $RESOURCE_GROUP -Name "deploymentstorageaccount" -Type Standard_LRS -Location $LOCATION
-$Deployment_Storage_Account | New-AzureStorageContainer -Name bosh -Permission Off
-$Deployment_Storage_Account | New-AzureStorageContainer -Name stemcell -Permission Off
+$Ops_Manegr_vhd = Set-AzureStorageBlobContent -Container $OpsManage_Container.Name `
+    -File  $HOME\Downloads\ops-manager-2.0-build.213.vhd -Context $Storage_Account.Context
+$Deployment_Storage_Account = New-AzureRmStorageAccount -ResourceGroupName $RESOURCE_GROUP `
+ -Name "deploymentstorageaccount" -Type Standard_LRS -Location $LOCATION
+$Deployment_bosh_container = $Deployment_Storage_Account | New-AzureStorageContainer -Name bosh -Permission Off
+$Deployment_stemcell_container = $Deployment_Storage_Account | New-AzureStorageContainer -Name stemcell -Permission Off
 
 
 $PublicIP = New-AzureRmPublicIpAddress -Name pcf-lb-ip -ResourceGroupName $RESOURCE_GROUP -Location $LOCATION -AllocationMethod Static
@@ -61,3 +67,16 @@ $pcf_lb = New-AzureRmLoadBalancer -Name pcf-lb -Location $LOCATION -ResourceGrou
 
 $Opsmgr_VIP = New-AzureRmPublicIpAddress -Name ops-manager-ip -ResourceGroupName $RESOURCE_GROUP -Location $LOCATION -AllocationMethod Static 
  ### opsmanager
+
+$vmName = "ops-manager-2.0"
+$computerName = "ops-manager-2"
+$vmSize = "Standard_DS1_v2"
+$location = $Location
+$imageName = "opsmanager"
+
+
+$imageConfig = New-AzureRmImageConfig -Location $location
+$imageConfig = Set-AzureRmImageOsDisk -Image $imageConfig -OsType Linux `
+ -OsState Generalized `
+ -BlobUri "https://boshstorageaccount.blob.local.azurestack.external/opsmanager/ops-manager-2.0-build.213.vhd"
+$image = New-AzureRmImage -ImageName $imageName -ResourceGroupName $rgName -Image $imageConfig
