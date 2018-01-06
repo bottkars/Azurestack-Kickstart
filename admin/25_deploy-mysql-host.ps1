5d75e742a075a8f61a92c8caaf73d578fc277eb4
Karsten Bott <Karsten.Bott@emc.com>
2018-01-06 18:54:04 +0100

    $templateuri

--- admin/25_deploy-mysql-host.ps1
+++ admin/25_deploy-mysql-host.ps1
@@ -6,13 +6,22 @@ $adminusername = $Global:MySQLRPadmin,
 )
 ,
 $RG = "rg_$mysqlhost"
-
+$templateuri = 'https://raw.githubusercontent.com/bottkars/AzureStack-QuickStart-Templates/patch-2/mysql-standalone-server-windows/azuredeploy.json'
+try {
+    Get-AzureRmVMImage -Location $Global:AZS_location -PublisherName MicrosoftWindowsServer `
+    -Offer WindowsServer -Skus 2016-Datacenter `
+    -ErrorAction Stop
+}
+catch {
+     Write-Warning "No 2016-Datacenter found in $($Global:AZS_location), please upload a 2016-Datacenter Image first ( use 11_deploy_windows_marketplace_image.ps1 )"
+     Break
+    }
 
 New-AzureRmResourceGroup -Name $RG -Location local 
 
 New-AzureRmResourceGroupDeployment -Name "$($mysqlhost)_deployment" -ResourceGroupName $RG `
     -vmName $mysqlhost `
-    -TemplateUri https://raw.githubusercontent.com/bottkars/AzureStack-QuickStart-Templates/patch-2/mysql-standalone-server-windows/azuredeploy.json `
+    -TemplateUri $templateuri `
     -adminPassword $adminpassword `
     -adminUsername $adminusername `
     -windowsOSVersion "2016-Datacenter" `
