57f863b3d89c0e25ccde40404c9511c7614c6b92
Karsten Bott <Karsten.Bott@emc.com>
2018-02-09 14:26:55 +0100

    script

--- admin/tools/ecs_pre.sh
+++ admin/tools/ecs_pre.sh
@@ -2,6 +2,13 @@
 yum install git pigz docker ntp firewalld -y
 yum remove *nfs* -y
 systemctl disable rpcbind
-shutdown -r now
+echo "${1}"
+
+if  [ "${1}" == "reboot" ];then
+        shutdown -r now
+else
+    sh ./ecs.sh
+fi
+
 
 
