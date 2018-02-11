c486771285687eadba5b6f5fed2a120264960666
Karsten Bott <Karsten.Bott@emc.com>
2018-02-11 11:02:07 +0100

    2>> /root/install.log

--- admin/emcecs/ecs_pre.sh
+++ admin/emcecs/ecs_pre.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-yum update -y
-yum install firewalld libselinux-python -y
-yum remove *nfs* -y
-systemctl disable rpcbind
+yum update -y 2>> /root/install.log  
+yum install firewalld libselinux-python -y 2>> /root/install.log  
+yum remove *nfs* -y 2>> /root/install.log  
+systemctl disable rpcbind 2>> /root/install.log  
 echo $?
