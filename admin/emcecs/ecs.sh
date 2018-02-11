7734e1b66d64653d9f68e2b837ab3a9ec20e8b99
Karsten Bott <Karsten.Bott@emc.com>
2018-02-11 11:09:45 +0100

    &>>

--- admin/emcecs/ecs.sh
+++ admin/emcecs/ecs.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 before_reboot(){
-yum update -y 2>> /root/install.log   
-yum install git firewalld -y 2>> /root/install.log
+yum update -y &>> /root/install.log   
+yum install git firewalld -y &>> /root/install.log
 systemctl disable rpcbind    
-cp ecs.sh /root/ 2>> /root/install.log
-chmod +X /root/ecs.sh 2>> /root/install.log
-chmod 755 /root/ecs.sh 2>> /root/install.log
-cp ecs-installer.service /etc/systemd/system/ 2>> /root/install.log
-systemctl daemon-reload 2>> /root/install.log
-systemctl enable ecs-installer.service 2>> /root/install.log
-git clone https://github.com/emcecs/ecs-communityedition /root/ECS-CommunityEdition 2>> /root/install.log
-cp deploy.yml /root/ECS-CommunityEdition 2>> /root/install.log
-echo $? 2>> /root/install.log
+cp ecs.sh /root/ &>> /root/install.log
+chmod +X /root/ecs.sh &>> /root/install.log
+chmod 755 /root/ecs.sh &>> /root/install.log
+cp ecs-installer.service /etc/systemd/system/ &>> /root/install.log
+systemctl daemon-reload &>> /root/install.log
+systemctl enable ecs-installer.service &>> /root/install.log
+git clone https://github.com/emcecs/ecs-communityedition /root/ECS-CommunityEdition &>> /root/install.log
+cp deploy.yml /root/ECS-CommunityEdition &>> /root/install.log
+echo $? &>> /root/install.log
 }
 
 after_bootstrap(){
     cd /root/ECS-CommunityEdition
-    /root/bin/step1 2>> /root/install.log
-    /root/bin/step2 2>> /root/install.log
+    /root/bin/step1 &>> /root/install.log
+    /root/bin/step2 &>> /root/install.log
 }
 
 after_reboot(){
--- admin/emcecs/ecs_pre.sh
+++ admin/emcecs/ecs_pre.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-yum update -y 2>> /root/install.log  
-yum install firewalld libselinux-python -y 2>> /root/install.log  
-yum remove *nfs* -y 2>> /root/install.log  
-systemctl disable rpcbind 2>> /root/install.log  
+yum update -y &>> /root/install.log  
+yum install firewalld libselinux-python -y &>> /root/install.log  
+yum remove *nfs* -y &>> /root/install.log  
+systemctl disable rpcbind &>> /root/install.log  
 echo $?
