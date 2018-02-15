ec42cfa6d48352f424b0cbe27289de7b03c2cd25
Karsten Bott <Karsten.Bott@emc.com>
2018-02-15 18:37:20 +0100

    test version for dynamic parameters

--- admin/emcecs/ecs.sh
+++ admin/emcecs/ecs.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 edit_template(){
+
 chars=( {c..z} )
 n=$1
 for ((i=0; i<n; i++))
 do
-    disks[i]=sd${chars[i]}
+    disks[i]="\/dev\/sd${chars[i]}"
 done
-disklist=$(echo "'${disks[*]}'" | tr ' ' ,)
-disklist=${disklist//","/"','"}
-echo "replacing mydisks with disklist $disklist"
-sed -i -e "s/mydisks/$disklist/g" /root/ECS-CommunityEdition/deploy.yml
+disklist="$(echo "'${disks[*]}'" | tr ' ' ,)"
+disklist="${disklist//","/"','"}"
+echo "replacing mydisks with disklist $disklist" >> /root/install.log
+sed -i -e 's/mydisks/'"$disklist"'/g' /root/ECS-CommunityEdition/deploy.yml
 
 n=$2
 for ((i=1; i<=n; i++))
 do
     hosts[i]=$3$i
 done
-hostlist=$(echo "'${hosts[*]}'" | tr ' ' ,)
-hostlist=${hostlist//","/"','"}
-echo "replacing myhosts with hostlist $hostlist"
-sed -i -e "s/myhosts/$hostlist/g" /root/ECS-CommunityEdition/deploy.yml
+hostlist="$(echo "'${hosts[*]}'" | tr ' ' ,)"
+hostlist="${hostlist//","/"','"}"
+echo "replacing myhosts with hostlist $hostlist" >> /root/install.log
+sed -i -e 's/myhosts/'"$hostlist"'/g' /root/ECS-CommunityEdition/deploy.yml
 
 n=$2
-for ((i=1; i<=n; i++))
+for ((i=4; i<=n+3; i++))
 do
     members[i]=10.0.0.$i
 done
-memberlist=$(echo "'${members[*]}'" | tr ' ' ,)
-memberlist=${memberlist//","/"','"}
-echo "replacing mymembers with memberlist $memberlist"
-sed -i -e "s/mymembers/$memberlist/g" /root/ECS-CommunityEdition/deploy.yml
+memberlist="$(echo "'${members[*]}'" | tr ' ' ,)"
+memberlist="${memberlist//","/"','"}"
+echo "replacing mymembers with memberlist $memberlist" >> /root/install.log
+sed -i -e 's/mymembers/'"$memberlist"'/g' /root/ECS-CommunityEdition/deploy.yml
 }
 before_reboot(){
 yum install git firewalld -y 
@@ -42,6 +43,7 @@ systemctl daemon-reload
 systemctl enable ecs-installer.service 
 git clone https://github.com/emcecs/ecs-communityedition /root/ECS-CommunityEdition 
 cp deploy.yml /root/ECS-CommunityEdition 
+echo "$1 $2 $3" /root/parameters.txt
 edit_template $1 $2 $3
 myreboot & 
 echo $? 
@@ -54,6 +56,7 @@ after_bootstrap(){
     cd /root/ECS-CommunityEdition
     /usr/bin/step1 |& tee -a /root/install.log
     /usr/bin/step2 |& tee -a /root/install.log
+    echo "done" |& tee -a /root/install.log
 }
 
 after_waagent(){
