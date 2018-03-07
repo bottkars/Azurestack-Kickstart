11de3bc86b26d94249df79daa5c336d8dd08f6f1
bottkars <Karsten.Bott@bottnet.de>
2018-03-07 07:04:28 +0100

    provider namespace

--- user/tools/get-azurermapiversions.ps1
+++ user/tools/get-azurermapiversions.ps1
@@ -1,5 +1,32 @@
+param(
+  [Parameter(ParameterSetName = "1", Mandatory = $true, Position = 1)][ValidateSet(
+'Microsoft.AzureBridge.Admin',
+'Microsoft.Backup.Admin',
+'Microsoft.Commerce.Providers',
+'Microsoft.Compute',
+'Microsoft.Compute.Admin',
+'Microsoft.Fabric.Admin',
+'Microsoft.Gallery.Providers',
+'Microsoft.InfrastructureInsights.Admin',
+'Microsoft.InfrastructureInsights.Providers',
+'Microsoft.Insights',
+'Microsoft.Insights.Providers',
+'Microsoft.KeyVault',
+'Microsoft.KeyVault.Admin',
+'Microsoft.Network',
+'Microsoft.Network.Admin',
+'Microsoft.Storage',
+'Microsoft.Storage.Admin',
+'Microsoft.Storage.Admin',
+'Microsoft.Subscriptions.Admin',
+'Microsoft.Subscriptions.Providers',
+'Microsoft.Update.Admin'
+  )]$providerNamespace
+
+)
+
 Get-AzureRmResourceProvider | `
   Select ProviderNamespace -Expand ResourceTypes | `
   Select * -Expand ApiVersions | `
   Select ProviderNamespace, ResourceTypeName, @{Name='ApiVersion'; Expression={$_}} | `
-  where-Object {$_.ProviderNamespace -like 'Microsoft.compute'}
+  where-Object {$_.ProviderNamespace -like $providerNamespace}
