fdb4d567cdbde90f2199cc6958ca8287c2fc4ba8
bottkars <karsten.bott@bottnet.de>
2018-03-07 18:24:36 +0100

    New repo

--- admin/27_deploy-mysql-hostingserver.ps1
+++ admin/27_deploy-mysql-hostingserver.ps1
@@ -17,7 +17,7 @@ $fqdn = "$mysqlhost.local.cloudapp.azurestack.external"
 New-AzureRmResourceGroup -Name $RG -Location local 
 
 New-AzureRmResourceGroupDeployment -Name MySQLHostingserver_Deploy -ResourceGroupName $RG `
-    -TemplateUri https://raw.githubusercontent.com/bottkars/AzureStack-QuickStart-Templates/patch-1/101-mysqladapter-add-hosting-server/azuredeploy.json `
+    -TemplateUri 'https://raw.githubusercontent.com/Azure/AzureStack-QuickStart-Templates/101-mysqladapter-add-hosting-server/azuredeploy.json' `
     -HostingServerName $fqdn `
 -password $MySQLRPPassword `
 -username $MySQLRPadmin `
