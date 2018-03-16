c09ea7fe544fd6f7b0e6c1ab12d5e66881b210b7
bottkars <Karsten.Bott@bottnet.de>
2018-03-16 08:47:01 +0100

    moved askd only stuff to xxx_asdk

--- Azurestack-Kickstart.ps1
+++ Azurestack-Kickstart.ps1
@@ -1,7 +1,7 @@
 
 <#PSScriptInfo
 
-.VERSION 2.0
+.VERSION 2.1
 
 .GUID a6511736-a96f-4c6f-a8f2-2f4f877627c0
 
@@ -37,18 +37,7 @@
 
 #> 
 [CmdletBinding(HelpUri = "https://github.com/bottkars/azurestack-kickstart")]
-param ([Parameter(ParameterSetName = "1", Mandatory = $false,Position = 1)][ValidateSet('af-ZA',
-'sq-AL','ar-DZ','ar-BH','ar-EG','ar-IQ','ar-JO','ar-KW','ar-LB','ar-LY','ar-MA','ar-OM','ar-QA','ar-SA','ar-SY','ar-TN','ar-AE','ar-YE',
-'hy-AM','Cy-az-AZ','Lt-az-AZ','eu-ES','be-BY','bg-BG','ca-ES','zh-CN','zh-HK','zh-MO','zh-SG','zh-TW','zh-CHS','zh-CHT','hr-HR','cs-CZ',
-'da-DK','div-MV','nl-BE','nl-NL',
-'en-AU','en-BZ','en-CA','en-CB','en-IE','en-JM','en-NZ','en-PH','en-ZA','en-TT','en-GB','en-US','en-ZW','et-EE',
-'fo-FO','fa-IR','fi-FI','fr-BE','fr-CA','fr-FR','fr-LU','fr-MC','fr-CH','gl-ES','ka-GE',
-'de-AT','de-DE','de-LI','de-LU','de-CH',
-'el-GR','gu-IN','he-IL','hi-IN','hu-HU','is-IS','id-ID','it-IT','it-CH','ja-JP','kn-IN','kk-KZ','kok-IN','ko-KR','ky-KZ','lv-LV','lt-LT','mk-MK','ms-BN','ms-MY','mr-IN','mn-MN',
-'nb-NO','nn-NO','pl-PL','pt-BR','pt-PT','pa-IN','ro-RO','ru-RU','sa-IN','Cy-sr-SP','Lt-sr-SP','sk-SK','sl-SI',
-'es-AR','es-BO','es-CL','es-CO','es-CR','es-DO','es-EC','es-SV','es-GT','es-HN','es-MX','es-NI','es-PA','es-PY','es-PE','es-PR','es-ES','es-UY','es-VE',
-'sw-KE','sv-FI','sv-SE','syr-SY','ta-IN','tt-RU','te-IN','th-TH','tr-TR','uk-UA','ur-PK','Cy-uz-UZ','Lt-uz-UZ','vi-VN'
-)]$LanguageTag = "en-US",
+param (
 [switch]$noutils
 )
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::TLS12 
@@ -72,16 +61,7 @@ if (!$myWindowsPrincipal.IsInRole($adminRole))
   }
 
 Set-Location $Home
-Write-Host -ForegroundColor White -NoNewline  "[==>]Disabling Windows Update"    
-Start-Process "sc" -ArgumentList "config wuauserv start=disabled" -Wait -NoNewWindow
-Write-Host -ForegroundColor Green "[Done]"
 
-Write-Host -ForegroundColor Gray "[==>]setting language to $LanguageTag"
-$Locale = $LanguageTag -replace "_","-"
-Set-Culture $Locale | Out-Null
-Set-WinSystemLocale $Locale | Out-Null
-Set-WinUserLanguageList -LanguageList $Locale -Force | Out-Null
-Write-Host -ForegroundColor Green "[Done]"
 
 if (!$noutils.IsPresent)
 {
---
+++ admin/tools/xxx_asdk_prep.ps1
@@ -0,0 +1,36 @@
+[CmdletBinding(HelpUri = "https://github.com/bottkars/azurestack-kickstart")]
+param ([Parameter(ParameterSetName = "1", Mandatory = $false,Position = 1)][ValidateSet('af-ZA',
+'sq-AL','ar-DZ','ar-BH','ar-EG','ar-IQ','ar-JO','ar-KW','ar-LB','ar-LY','ar-MA','ar-OM','ar-QA','ar-SA','ar-SY','ar-TN','ar-AE','ar-YE',
+'hy-AM','Cy-az-AZ','Lt-az-AZ','eu-ES','be-BY','bg-BG','ca-ES','zh-CN','zh-HK','zh-MO','zh-SG','zh-TW','zh-CHS','zh-CHT','hr-HR','cs-CZ',
+'da-DK','div-MV','nl-BE','nl-NL',
+'en-AU','en-BZ','en-CA','en-CB','en-IE','en-JM','en-NZ','en-PH','en-ZA','en-TT','en-GB','en-US','en-ZW','et-EE',
+'fo-FO','fa-IR','fi-FI','fr-BE','fr-CA','fr-FR','fr-LU','fr-MC','fr-CH','gl-ES','ka-GE',
+'de-AT','de-DE','de-LI','de-LU','de-CH',
+'el-GR','gu-IN','he-IL','hi-IN','hu-HU','is-IS','id-ID','it-IT','it-CH','ja-JP','kn-IN','kk-KZ','kok-IN','ko-KR','ky-KZ','lv-LV','lt-LT','mk-MK','ms-BN','ms-MY','mr-IN','mn-MN',
+'nb-NO','nn-NO','pl-PL','pt-BR','pt-PT','pa-IN','ro-RO','ru-RU','sa-IN','Cy-sr-SP','Lt-sr-SP','sk-SK','sl-SI',
+'es-AR','es-BO','es-CL','es-CO','es-CR','es-DO','es-EC','es-SV','es-GT','es-HN','es-MX','es-NI','es-PA','es-PY','es-PE','es-PR','es-ES','es-UY','es-VE',
+'sw-KE','sv-FI','sv-SE','syr-SY','ta-IN','tt-RU','te-IN','th-TH','tr-TR','uk-UA','ur-PK','Cy-uz-UZ','Lt-uz-UZ','vi-VN'
+)]$LanguageTag = "en-US"
+)
+
+Write-Host -ForegroundColor White -NoNewline  "[==>]Disabling Windows Update"    
+Start-Process "sc" -ArgumentList "config wuauserv start=disabled" -Wait -NoNewWindow
+Write-Host -ForegroundColor Green "[Done]"
+
+Write-Host -ForegroundColor Gray "[==>]setting language to $LanguageTag"
+$Locale = $LanguageTag -replace "_","-"
+Set-Culture $Locale | Out-Null
+Set-WinSystemLocale $Locale | Out-Null
+Set-WinUserLanguageList -LanguageList $Locale -Force | Out-Null
+Write-Host -ForegroundColor Green "[Done]"
+$AZDCredential = Get-Credential -Credential "Azurestack\AzurestackAdmin" -Message "Enter AD Credentials for AzureStackAdmin"
+$AZSvms = get-vm -Name AZS*
+$scriptblock = {
+$env:COMPUTERNAME    
+sc.exe config wuauserv start=disabled
+Get-Service -Name wuauserv | fl StartType,Status,PSremote
+}
+foreach ($vm in $AZSvms) {
+Invoke-Command -VMName $vm.name -ScriptBlock $scriptblock -Credential $AZDCredential
+}
+sc.exe config wuauserv start=disabled
