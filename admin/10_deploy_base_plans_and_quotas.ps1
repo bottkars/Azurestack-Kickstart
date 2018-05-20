79376ad05636b63df368050f0684c347290d7027
bottkars <Karsten.Bott@bottnet.de>
2018-05-20 03:15:10 +0200

    add rg to base plans

--- admin/10_deploy_base_plans_and_quotas.ps1
+++ admin/10_deploy_base_plans_and_quotas.ps1
@@ -17,6 +17,11 @@ if (!$Global:Service_RM_Account.Context)
     }
 $name = $plan_name
 $rg_name = "plans_and_offers"
+if (!$RG = Get-AzureRmResourceGroup -Name $rg_name -Location local)
+    {
+    New-AzureRmResourceGroup -Name $rg_name -Location local   
+    }
+
 Write-Host -ForegroundColor White -NoNewline "Creating Quota $($name)_compute"
 $ComputeQuota = New-AzsComputeQuota -Name "$($name)_compute" -Location local # -VirtualMachineCount 5000
 Write-Host -ForegroundColor Green [Done]
