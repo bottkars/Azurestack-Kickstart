331adcb5707d5bd0559d2199aeb0a77a57f6076e
bottkars <Karsten.Bott@bottnet.de>
2018-05-28 09:43:18 +0200

    tweaked pcf

--- pcf/admin/01_create_pcf_offer.ps1
+++ pcf/admin/01_create_pcf_offer.ps1
@@ -1,3 +1,12 @@
+$rg_name = "plans_and_offers"
+if (!($RG = Get-AzureRmResourceGroup -Name $rg_name -Location local))
+    {
+    Write-Host -ForegroundColor White -NoNewline "Creating RG $rg_name"        
+    $TG = New-AzureRmResourceGroup -Name $rg_name -Location local
+    Write-Host -ForegroundColor Green [Done]
+    }
+
+
 $ComputeQuota = New-AzsComputeQuota -Name best-compute `
  -Location local -VirtualMachineCount 5000 `
  -AvailabilitySetCount 20 -CoresLimit 100 -VmScaleSetCount 20
@@ -9,6 +18,9 @@ $StorageQuota = New-AzsStorageQuota -Name best-storage -Location local `
  -NumberOfStorageAccounts 300 -CapacityInGB 50000 
 
 ## create a plan
-$PCF_PLAN = New-AzsPlan -Name best-plan -DisplayName "Best-plan for pcf /cf" -ResourceGroupName "palns_and_quotas" -QuotaIds $StorageQuota.Id,$NetworkQuota.Id,$ComputeQuota.Id -ArmLocation local
-$Offer = New-AzsOffer -Name best-offer -DisplayName "Offer for PCF / Cloud Foundry" -BasePlanIds $PCF_PLAN.Id -ArmLocation local -ResourceGroupName "pfc-offer-rg"
-New-AzsTenantSubscription -DisplayName "Azure PCF Subscription" -Owner pcfuser@labbuildr.com -OfferId $Offer.Id 
+$PCF_PLAN = New-AzsPlan -Name best-plan -DisplayName "Best-plan for pcf /cf" `
+-ResourceGroupName $rg_name `
+-QuotaIds $StorageQuota.Id,$NetworkQuota.Id,$ComputeQuota.Id -ArmLocation local
+$Offer = New-AzsOffer -Name best-offer -DisplayName "Offer for PCF / Cloud Foundry" `
+ -BasePlanIds $PCF_PLAN.Id -State Private -ArmLocation local -ResourceGroupName $rg_name
+New-AzsTenantSubscription -DisplayName "Azure PCF Subscription" -Owner $Global:Service_RM_Account.Context.Account.Id -OfferId $Offer.Id 
