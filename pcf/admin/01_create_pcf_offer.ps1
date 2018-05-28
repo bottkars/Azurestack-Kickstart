0b4ddf450b2f5e598a7948b507ccab921251a652
bottkars <Karsten.Bott@bottnet.de>
2018-05-28 09:52:25 +0200

    AZS User Subscription

--- pcf/admin/01_create_pcf_offer.ps1
+++ pcf/admin/01_create_pcf_offer.ps1
@@ -18,9 +18,9 @@ $StorageQuota = New-AzsStorageQuota -Name best-storage -Location local `
  -NumberOfStorageAccounts 300 -CapacityInGB 50000 
 
 ## create a plan
-$PCF_PLAN = New-AzsPlan -Name best-plan -DisplayName "Best-plan for pcf /cf" `
+$PCF_PLAN = New-AzsPlan -Name PCF-Plan -DisplayName "plan for pcf /cf" `
 -ResourceGroupName $rg_name `
 -QuotaIds $StorageQuota.Id,$NetworkQuota.Id,$ComputeQuota.Id -ArmLocation local
 $Offer = New-AzsOffer -Name best-offer -DisplayName "Offer for PCF / Cloud Foundry" `
  -BasePlanIds $PCF_PLAN.Id -State Private -ArmLocation local -ResourceGroupName $rg_name
-New-AzsTenantSubscription -DisplayName "Azure PCF Subscription" -Owner $Global:Service_RM_Account.Context.Account.Id -OfferId $Offer.Id 
+New-AzsUserSubscription -DisplayName "Azure PCF Subscription" -Owner $Global:Service_RM_Account.Context.Account.Id -OfferId $Offer.Id 
