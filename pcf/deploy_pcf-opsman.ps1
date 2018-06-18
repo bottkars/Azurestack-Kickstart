4177653012f48da4c11dd3a680562afb37c847df
bottkars <Karsten.Bott@bottnet.de>
2018-06-18 14:58:15 +0200

    replace

--- pcf/deploy_pcf-opsman.ps1
+++ pcf/deploy_pcf-opsman.ps1
@@ -15,7 +15,7 @@ $dnsZoneName = "pcfpas.local.azurestack.external",
 [switch]$RegisterProviders,
 [switch]$OpsmanUpdate
 )
-$storageaccount = ($resourceGroup+$Storageaccount)
+$storageaccount = ($resourceGroup+$Storageaccount) -Replace '[^a-zA-Z]',''
 $storageaccount = $Storageaccount.subString(0,[System.Math]::Min(24, $storageaccount.Length))
 $vhdName = 'image.vhd'
 $storageType = 'Standard_LRS'
